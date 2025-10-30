fbfda970c1c830e8cb0a79dafccc41c293937906
adonzalenko <adonzalenko@gmail.com>
2025-10-30 21:48:24 +0700

    Исправление кода в calculate

--- test_project/calculate.py
+++ test_project/calculate.py
@@ -6,18 +6,25 @@
 #M - целое число
 #T - целое число
 #Вывод в виде: 08:05
+
 n = int(input())
 m = int(input())
 t = int(input())
+
 days = t // 1440
 hours = t // 60 - 24 * days
+
 resulthours = n + hours
 resultminutes = m + (t - t // 60 * 60)
+
 if resulthours > 24:
     resulthours = resulthours - 24
+
 if resultminutes > 60:
     resultminutes = resultminutes - 60
     resulthours = resulthours + 1
+
     print(f'{resulthours:0>2}:{resultminutes:0>2}')
+
 else:
     print(f'{resulthours:0>2}:{resultminutes:0>2}')

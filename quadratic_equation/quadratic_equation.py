43aa54ad95379d2efe9b2aa1a49f17a677dfdb9d
adonzalenko <adonzalenko@gmail.com>
2025-11-25 19:44:04 +0700

    Использовала сравнение с эпсилон, а не с 0. Исправила логику проверки условий коэффициентов. А также сравнение дискриминанта. В уравнении поиска дикриминанта ошибочно стояли скобки.

--- quadratic_equation/quadratic_equation.py
+++ quadratic_equation/quadratic_equation.py
@@ -1,32 +1,26 @@
 from math import sqrt
+from math import fabs
 
 ratio_a = float(input("<Введите коэффициент а: "))
 ratio_b = float(input("<Введите коэффициент b: "))
 ratio_c = float(input("<Введите коэффициент c: "))
 
-if ratio_a == 0:
-    if ratio_c == 0:
-        print("Любое число является решением")
-    else:
-        if ratio_b != 0:
-            equation_solving = -ratio_c / ratio_b
-            print(f"Корень уравнения = {equation_solving}")
+epsilon = 1.0e-10
+
+if fabs(ratio_a) < epsilon:
+    if fabs(ratio_b) < epsilon:
+        if fabs(ratio_c) < epsilon:
+            print("Любое число является решением")
         else:
             print("Уравнение неразрешимо")
-elif ratio_b == 0:
-    if ratio_a != 0:
-        if ratio_c >= 0:
-            equation_solving_2 = sqrt(-ratio_c / ratio_a)
-            print(f"Корень уравнения =+- {equation_solving_2}")
-        else:
-            print("Реальный корней нет")
     else:
-        print("Коэффициент а не может быть равен 0")
+        equation_solving = -ratio_c / ratio_b
+        print(f"Корень уравнения = {equation_solving}")
 else:
-    result_d = ratio_b ** 2 - (4 * ratio_a * ratio_c)
-    if result_d < 0:
+    result_d = ratio_b ** 2 - 4 * ratio_a * ratio_c
+    if result_d <= -epsilon:
         print("Корней нет")
-    elif result_d == 0:
+    elif fabs(result_d) < epsilon:
         equation_root = -ratio_b / (2 * ratio_a)
         print(f"Корень уравнения = {equation_root:.2f} ")
     else:

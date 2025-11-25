69f0270fe559f348ef4076af187e562d5398cc77
adonzalenko <adonzalenko@gmail.com>
2025-11-25 18:31:43 +0700

    Поправила проверку високосного года.

--- find_date/find_date.py
+++ find_date/find_date.py
@@ -2,14 +2,15 @@ day = int(input("Введите день: "))
 month = int(input("Введите месяц: "))
 year = int(input("Введите год: "))
 
-check_year = year % 4
+def is_leap(y):
+    return y % 4 == 0 and (y % 100 != 0 or y % 400 == 0)
 
-if day >31 or day <1 or month >12 or month <1 or year <1:
+if day > 31 or day < 1 or month > 12 or month < 1 or year < 1:
     print("Введите корректную дату")
     exit()
 
 if month == 2:
-    if check_year == 0:
+    if is_leap(year):
         if day > 29:
             print("Введите корректную дату")
             exit()
@@ -17,12 +18,9 @@ if month == 2:
         if day > 28:
             print("Введите корректную дату")
             exit()
+
 elif month in [4, 6, 9, 11]:
     if day > 30:
-        print("Введите корректную дату")
-        exit()
-else:
-    if day > 31:
         print("Введите корректную дату")
         exit()
 
@@ -31,8 +29,14 @@ next_month = month
 next_year = year
 
 if month == 2:
-    if check_year == 0:
+    if is_leap(year):
         if day == 29:
+            next_day = 1
+            next_month = 3
+        else:
+            next_day = day + 1
+    else:
+        if day == 28:
             next_day = 1
             next_month = 3
         else:

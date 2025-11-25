4d4f92983c3850541a187b98b27076b3494ed4d3
adonzalenko <adonzalenko@gmail.com>
2025-11-25 19:54:43 +0700

    убрала remainder, скорректировала проверку на високосность

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -1,13 +1,13 @@
-from math import remainder
+
 
 entered_year = int(input("Введите год для проверки: "))
-entered_year_remainder = remainder(entered_year, 4)
+leap_year = entered_year % 4 == 0 and (entered_year % 100 != 0 or entered_year % 400 == 0)
 
 if entered_year <= 0:
     print("Введите корректный год")
     exit()
 
-if entered_year_remainder == 0:
+if leap_year:
     print("Введенный год високосный")
 else:
     print("Введенный год не високосный")

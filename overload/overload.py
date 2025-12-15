f3b4ae02b1e1030b4f8d9db4fbf763003ce83d08
adonzalenko <asonzalenko@gmail.com>
2025-12-15 22:16:04 +0700

    Просьба обратить на комментарий TODO

--- overload/overload.py
+++ overload/overload.py
@@ -1,5 +1,3 @@
-
-
 def get_type_name(argument):
     if isinstance(argument, int):
         return 'Целое число'
@@ -9,12 +7,14 @@ def get_type_name(argument):
         return 'Комплексное число'
     elif isinstance(argument, str):
         return 'Строка'
-    elif argument(True or False):
+    elif argument(argument, bool):
         return 'Логическое значение'
     else:
         return 'Неизвестный тип'
 
 print(get_type_name('Привет'))
 print(get_type_name(2.5))
-print(get_type_name(False))
+print(get_type_name(False)) #неправильно работает тип bool
 print(get_type_name(4))
+
+#TODO По уссловия задачи требуется решить задачи с помощью multiple dispatch, но такой библиотеки нет
